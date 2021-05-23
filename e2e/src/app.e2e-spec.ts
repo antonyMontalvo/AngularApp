53ef08a21059d089d792903b3e09dbaffac81fb7
Antony Montalvo <antonis162010@gmail.com>
2021-05-23 13:55:15 -0500

    Update angular 7 to 10

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to AngularApp!');
+    expect(page.getTitleText()).toEqual('AngularApp app is running!');
   });
 
   afterEach(async () => {
